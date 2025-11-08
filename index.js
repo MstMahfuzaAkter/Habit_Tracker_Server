f206ccb86a41bf04cecf2ef81432361683e74036
MstMahfuzaAkter <mstmahfuzaker581@gamil.com>
2025-11-08 23:22:20 +0600

    added mongodb

--- index.js
+++ index.js
@@ -2,13 +2,38 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ruwopzq.mongodb.net/?appName=Cluster0`;
 
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
