1d02c8c6dd29601e60327c25cde5b21a6d101fff
MstMahfuzaAkter <mstmahfuzaker581@gamil.com>
2025-11-10 19:04:58 +0600

    added get data from db

--- index.js
+++ index.js
@@ -19,14 +19,21 @@ const client = new MongoClient(uri, {
     }
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const db = client.db("habit-db");
+const habitCollection = db.collection("habit");
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+app.get("/habit", async (req, res) => {
+    const result = await habitCollection.find().toArray();
+    res.send(result);
+});
 
 async function run() {
     try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
+        //await client.connect();
+        //await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
